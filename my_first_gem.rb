2b88036f4872a25f37e5b65f1fc189b48f811980
Maryam Syed <maryamsyed2096@gmail.com>
2014-10-05 19:18:49 -0700

    formally running through the git tutorial book as well as committing some shiny ruby

---
+++ my_first_gem.rb
@@ -0,0 +1,10 @@
+# Well, this is not actually my first code in Ruby
+# I completed the Codecademy Ruby course!
+# So let's make a nice Ruby file and play with git and commit it.
+# I know I need to get better at writing useful comments as well. 
+# Generally I don't write any!
+
+def gem
+    puts "This is an awesome gem!"
+end 
+
